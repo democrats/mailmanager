f659bce5af5cc158d7fd71551fd5f7b31b103769
Wes Morgan <cap10morgan@gmail.com>
2011-01-12 13:17:33 -0500

    basic method proxy into withlist working

--- lib/mailmanager/listproxy.py
+++ lib/mailmanager/listproxy.py
@@ -10,6 +10,17 @@ class MailingListEncoder(json.JSONEncoder):
 def dumplist(mlist):
   print json.dumps(mlist, True, cls=MailingListEncoder)
 
+def command(mlist, cmd, *args):
+  try:
+    method = getattr(mlist, cmd)
+    print json.dumps(method(*args))
+  except TypeError as err:
+    error_msg = '%s' % err
+    print json.dumps({'error': error_msg})
+  except AttributeError as err:
+    error_msg = '%s is not a valid command; must be a MailList method' % err
+    print json.dumps({'error': error_msg})
+
 #def loadlist(mlist, jsonlist):
   #newlist = json.loads(jsonlist)
   #for attr in newlist:
